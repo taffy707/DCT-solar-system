a7067edb0597d8b4f0840a66ce3a490a1ac82aab
JoshuaHarris1107 <joshuaharris1107@outlook.com>
2024-08-09 16:09:23 +0200

    Added Encryption Libraries & files
    Added necessary Encryption and Cryptography libraries and provided the Python script which will connect to the MySQL Database.

--- dbConnector.py
+++ dbConnector.py
@@ -1,4 +1,12 @@
 import mysql.connector
+# from Crypto.Cipher import DES3
+# from Crypto.Util.Padding import pad, unpad
+
+# def encrypt(data, key):
+  #  cipher = DES3.new(key, DES3.MODE_ECB)
+   # padded_data = pad(data.encode(), DES3.block_size)
+   # encrypted_data = cipher.encrypt(padded_data)
+   # return 
 
 def connect_to_database(host, user, password, database):
     try:
@@ -39,5 +47,6 @@ def main():
                 print("No results found")
             SolarDB.close()
     
+    # Running Main Func
     if __name__ == "__main__":
         main()
